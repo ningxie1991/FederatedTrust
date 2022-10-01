d7fafc0202335960031a27ad282464a9eeb955af
ningxie1991 <ningxie1991@gmail.com>
2022-10-01 11:35:52 +0200

    added assets and refactored the pillar module

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import json
+import logging
 import os
 
-from accountability.assessment import document_completeness
+from algorithm.pillar import TrustPillar
 
 dirname = os.path.dirname(__file__)
+logging.basicConfig(filename='exp/log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 def assess():
-    print(f'Assessing model')  # Press Ctrl+F8 to toggle the breakpoint.
-    with open(os.path.join(dirname, 'assets/factsheet/example/factsheet.json'), 'r') as f, \
-        open(os.path.join(dirname, 'assets/factsheet/test/factsheet_missing_properties.json'), 'r') as t:
+    print(f'Assessing model \n--------------------')  # Press Ctrl+F8 to toggle the breakpoint.
+    factsheet_path = os.path.join(dirname, 'configs/factsheet/example/factsheet.json')
+    metrics_cfg_path = os.path.join(dirname, 'configs/metrics.json')
+
+    with open(factsheet_path, 'r') as f, \
+         open(metrics_cfg_path, 'r') as m:
         factsheet = json.load(f)
-        incomplete_factsheet = json.load(t)
-        document_completeness(factsheet)
-        document_completeness(incomplete_factsheet)
+        metrics_cfg = json.load(m)
+        input_docs = {'factsheet': factsheet}
+        for key, value in metrics_cfg.items():
+            pillar = TrustPillar(key, value, input_docs)
+            score = pillar.evaluate()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
     assess()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
